397b100f938d9c8bef0d2cf26414eeee8573f2e0
Vladimir Psyukalov <85486404+cyberdev-ios@users.noreply.github.com>
2021-08-20 13:41:46 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,13 +2,13 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
-let realmVersionStr = "3.20.0"
-let objectMapperVersionStr = "3.5.1"
+let realmVersionStr = "10.7.2"
+let objectMapperVersionStr = "3.5.3"
 
 let package = Package(
     name: "ObjectMapper+Realm",
     platforms: [
-        .iOS(.v8),
+        .iOS(.v11),
         .tvOS(.v10),
         .macOS(.v10_10),
     ],
@@ -19,8 +19,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-cocoa.git", .exact(Version(realmVersionStr)!)),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .exact(Version(objectMapperVersionStr)!)),
+        .package(url: "https://github.com/realm/realm-cocoa.git", .exact(.init(stringLiteral: realmVersionStr))),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .exact(.init(stringLiteral: objectMapperVersionStr)))
     ],
     targets: [
         .target(
